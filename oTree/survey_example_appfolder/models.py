9b4ff61d4171e9f714b404b34165eaebe898f029
malaugonzalezp <malaugonzalezp@gmail.com>
2022-06-07 11:52:48 +0200

    hw2

--- oTree/survey_example_appfolder/models.py
+++ oTree/survey_example_appfolder/models.py
@@ -9,6 +9,11 @@ from otree.api import (
     currency_range,
 )
 
+
+import random
+
+from survey_example_appfolder.HelperFunctions import random_number
+
 author = 'My name is Maria Gonzalez'
 doc = 'Test for implementing an online survey in oTree'
 
@@ -20,7 +25,16 @@ class Constants(BaseConstants):
 
 
 class Subsession(BaseSubsession):
-    pass
+    def creating_session(self):
+        '''this is a function by otree (same can not be changed)
+        which is creating a new subsession. Any variables that are needed to be custom
+        (so declaring it in a different way before) are created here'''
+        for p in self.get_players():
+            #here we want to declare the players to different groups (2 in total)
+            #we use a python function here from 'random' we imported earlier
+            p.group_assignment = random.Random().randint(0, 1)
+            #or:
+            #p.group_assignment = random_number(0,2)
 
 
 class Group(BaseGroup):
@@ -33,10 +47,25 @@ class Player(BasePlayer):
     # this is the most important feature of this file. We can collect all the variables used on the html pages here
 
     # The Variables are structured on the base of pages
+    # WelcomePage
+    device_type = models.IntegerField(initial=-999)
+    operating_system = models.IntegerField(initial=-999)
+    screen_height = models.IntegerField(initial=-999)
+    screen_width = models.IntegerField(initial=-999)
+    #HtmlPage
+    duck = models.IntegerField(initial=-999)
+    # DemoPage
     please_state_your_age = models.IntegerField(max=110, min=1)  # we can also have max and min guidelines
     what_is_your_favorite_beverage = models.StringField(blank=True)  # this is an optional field through blank = True
     food = models.IntegerField(initial=-999)  # we can add an initial value
     hidden_input = models.IntegerField(initial=50, blank=True)
+    # PopoutPage
+    popout_question = models.IntegerField(blank=True)
+    popout_yes = models.StringField(blank=True)
+    popout_no = models.StringField(blank=True)
+    time_popout = models.StringField(initial='-999')
+    # EndPage
+    group_assignment = models.IntegerField()
 
     # custom error message
     # has to:
--- oTree/survey_example_appfolder/pages.py
+++ oTree/survey_example_appfolder/pages.py
@@ -9,6 +9,7 @@ from .models import Constants, Player
 
 class Welcome(Page):
     form_model = Player
+    form_fields = ['device_type', 'operating_system', 'screen_height', 'screen_width']
 
 #with the function before_next_page you can can control what should happen. It is a nice feature for filtering
 #or also setting variables
@@ -21,11 +22,33 @@ class DemoPage(Page):
     form_model = Player
     form_fields = ['please_state_your_age', 'what_is_your_favorite_beverage', 'food', 'hidden_input']
 
+class Html_overview(Page):
+    form_model = Player
+    form_fields = ['duck']
+    def is_displayed(self):
+        '''this is another otree specific function that regulates if a page is displayed or not '''
+        #this will show the page to anybody who has the right assignment so in this case
+        return self.player.group_assignment == 1
+
+
+class PopoutPage(Page):
+    form_model = Player
+    form_fields = ['popout_question', 'popout_yes', 'popout_no', 'time_popout']
+
+
 class EndPage(Page):
-    #style: this is a good example of the style 'CamelCase' that one normally uses for classes
+    def vars_for_template(self):
+        '''this is another function by otree which allows you to "send" variables
+        to html files if you need to access them from there'''
+        return {"group_assignment": safe_json(self.player.group_assignment)}
+
+    # style: this is a good example of the style 'CamelCase' that one normally uses for classes
     form_model = Player
 
-#Here we define in which ordering we want the pages to be shown. We always start with a Welcome page and end with an End page.
+
+# Here we define in which ordering we want the pages to be shown. We always start with a Welcome page and end with an End page.
 page_sequence = [Welcome,
-                DemoPage,
-                EndPage]
+                 DemoPage,
+                 Html_overview,
+                 PopoutPage,
+                 EndPage]
